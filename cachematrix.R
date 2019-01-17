db446ac489fc1a0cfd678408433c178c651d43ea
Samantha <46547820+batchesr@users.noreply.github.com>
2019-01-16 19:55:52 -0500

    Added more descriptive comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
         i<-NULL
         
-set<-function(y=matrix()) {
+set<-function(y=matrix()) {             ## Set function caches the matrix y to x
         x<<-y
         i<<-NULL
 }
-get<-function() {
+get<-function() {                       ## Get function returns back the matrix x that has been cached
         return(x)        
 }
-setInv<-function(inv) {
+setInv<-function(inv) {                 ## SetInv function caches the inverse matrix of x
         i<<-inv
 }
-getInv<-function() {
+getInv<-function() {                    ## GetInv function returns back the cached inverse
         return(i)
 }
 list(set=set,get=get,setInv=setInv,getInv=getInv)
@@ -24,14 +24,14 @@ list(set=set,get=get,setInv=setInv,getInv=getInv)
 
 cacheSolve <- function(x, ...)
 {
-        i<-x$getInv()
-        if(!is.null(i))
-        {
+        i<-x$getInv()                   ## Calls back the cached inverse matrix from above
+        if(!is.null(i))                 ## If the called back matrix exists, it will print "getting cached matrix"
+        {                               ## and return the inversed matrix
                 message("getting cached data")
                 return(i)
         }
-        data<-x$get()
-        i<-solve(data,...)
+        data<-x$get()                   ## If not, calls back the original matrix and solves for the inverse
+        i<-solve(data,...)              ## using the solve function (for square matrices)
         x$setInv(i)
-        i
+        i                               ## Returns the solved inverse matrix
 }
